566b10d9dd0bece628bb9b823f0043ddb569c477
Prateek Kumar <prateekk268@gmail.com>
2023-05-28 21:43:30 +0530

    fix-some-error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const route = require("./src/route/route")
-
+require('dotenv').config()
 
 
 app.use(express.json())
@@ -10,28 +10,26 @@ app.use(express.urlencoded({extended:true}))
 
 
 const DBConnection = async () => {
-    const MONGO_URI = "mongodb+srv://prateek:cvKx4jrPzfgmmbUT@mern.483njbl.mongodb.net/?retryWrites=true&w=majority"; 
+    // const MONGO_URI = "mongodb+srv://prateek:cvKx4jrPzfgmmbUT@mern.483njbl.mongodb.net/?retryWrites=true&w=majority"; 
+    const Data = process.env.MONGO_URI;
+    // console.log(Data.toString())
     try {
-        await mongoose.connect(MONGO_URI,{dbName: "wowalent", useNewUrlParser: true});
+        await mongoose.connect(Data,{dbName: "wowalent", useNewUrlParser: true});
         console.log("Database connected successfully");
     } catch (error) {
-        console.log("Error while connecting with the database ", error.message);
+        console.log("Error while connecting with the database ", error);
     }
 }
 DBConnection()
 
 
-app.use('/', route)
+app.use('/', route);
 
 app.all('*', function (req, res) {
-    throw new Error("You Hit Wrong Api!!!, Plz Check !!!");
+    return res.status(404).send("Welcome, look like you hit the wrong Api");
 });
 
-app.use(function (e, req, res, next) {
-    if (e.message === "You Hit Wrong Api!!!, Plz Check !!!") {
-       throw new Error(message);
-    }
-});
+
 
 app.listen(process.env.PORT || 5000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 5000))
